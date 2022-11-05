d4cab6067c1c84ac12217e5b8f7fe0c224b75e2c
RaeesaParker <raeesaparker101@gmail.com>
2022-11-05 12:35:28 +0000

    added add note functionality

--- src/components/subComponents/CurrentNotes.vue
+++ src/components/subComponents/CurrentNotes.vue
@@ -1,7 +1,19 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, reactive} from 'vue';
   import InsertNote from './InsertNote.vue';
   import CreateNote from './CreateNote.vue';
+
+
+  // Create an array to store the notes
+  let noteList = ref([  
+  ]);
+
+  //  Function to add a new note => takes created note and pushes to the notelist
+  const addNoteFunc = (note) => {
+    noteList.value = [...noteList.value, { noteTitle: note.noteTitle, noteContent: note.noteContent }]
+    console.log(note.note)
+    console.log(noteList.value)
+  };
 
 
 
@@ -10,8 +22,9 @@
 <template>
   <div class="section-insert">
 
-    <InsertNote> </InsertNote>
-  <CreateNote title="Hello" body="something"> </CreateNote>
+  <InsertNote @onAdd=addNoteFunc > </InsertNote>
+
+  <CreateNote v-for="(item, index) in noteList" :key="index" :noteItem="item" > </CreateNote>
 </div>
  
 </template>
